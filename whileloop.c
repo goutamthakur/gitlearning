559d706bc7b5cb3fe0191f52ace44006cdfd6165
goutamthakur <goutamthakur2508@gmail.com>
2021-07-09 17:27:49 +0530

    Added whileloop

---
+++ whileloop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    i = 1;
+    printf("Enter a number a less than or equal to 20: ");
+    scanf("%d", &n);
+    if (n > 20)
+    {
+        printf("\n");
+        printf("Entered number is out of range!!!\n");
+        printf("\n");
+
+    }
+    else
+    {
+        while (i <= n)
+        {
+            printf("%d\n", i);
+            i++;
+        }
+    }
+
+    return 0;
+}
