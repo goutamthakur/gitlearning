8f2babb1824ebafa34f07df5c7189fab7fb4e377
goutamthakur <goutamthakur2508@gmail.com>
2021-07-22 18:54:57 +0530

    Program with strcmp() function

---
+++ c programs/string and array in c/string_7.c
@@ -0,0 +1,26 @@
+// strcmp() function in  c 
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[100], b[100];
+    printf("Enter the first sentence:\n");
+    gets(a);
+    printf("Enter the second sentence which you want to compare with first sentence:\n");
+    gets(b);
+    int n = strcmp(a,b);
+    printf("Value after comparing is %d\n", n);
+    if (n==0)
+    {
+        printf("Both the entered sentence are same\n");
+    }
+    else
+    {
+        printf("Both the entered sentence are not same\n");
+        
+    }
+    
+    
+    return 0;
+
+}
