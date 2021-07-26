333fb153bb168be14798dbe9589800915838d227
goutamthakur <goutamthakur2508@gmail.com>
2021-07-26 21:40:31 +0530

    strcat() function in c

--- c programs/string and array in c/string_9.c
+++ c programs/string and array in c/string_9.c
@@ -1,2 +1,13 @@
 //strchr(str, ch) function in c
-// this programs is incomplete complete it
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    const char str[]= "This is just a String";
+    const char ch ='u';
+    char *p;
+    p = strchr(str, ch);
+    printf("String starting from %c is: %s ", ch, str);
+
+    return 0;
+}
