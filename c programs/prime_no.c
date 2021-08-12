95c006c7cdd9a446251d1725b79b2ed85f4f9de7
goutamthakur <goutamthakur2508@gmail.com>
2021-08-12 23:52:11 +0530

    Prime number program added

---
+++ c programs/prime_no.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, c = 0;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            c++;
+        }
+    }
+    if (c == 2)/*Since i starts from 1, and prime number is divible by both one and itself.
+                So for a prime number c will be increment by 2 */
+    {
+        printf("%d is a prime number", n);
+    }
+    else
+    {
+        printf("%d is not a prime number", n);
+    }
+
+    return 0;
+}
