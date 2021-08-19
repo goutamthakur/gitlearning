0ed8922af8bdcc88d515691145dd8ba8dd0072cc
goutamthakur <goutamthakur2508@gmail.com>
2021-08-19 23:59:11 +0530

    Added armstrong program

---
+++ c programs/armstrong.c
@@ -0,0 +1,27 @@
+//To find the given number is armstrong or not.
+#include <stdio.h>
+
+int main(){
+    int num, r, sum=0, temp;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while(num>0){
+        r=num%10;
+        sum=sum+(r*r*r);
+        num=num/10;
+    }
+   if (temp == sum)
+   {
+       printf("Entered number is an armstrong");
+   }
+   else
+   {
+       printf("Entered number is not an armstrong");
+       
+   }
+   
+   
+
+    return 0;
+}
