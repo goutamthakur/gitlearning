2fd1d367058cdf3c99b04080a13922e405c8cefd
goutamthakur <goutamthakur2508@gmail.com>
2021-09-13 00:33:12 +0530

    sizeof programs added

---
+++ c programming/pointers in c/sizeof_1.c
@@ -0,0 +1,21 @@
+//Operand is data type
+#include <stdio.h>
+
+int main()
+{
+    int a = 89;
+    
+    printf("Size of a variable is %d\n", sizeof(a));
+    printf("Size of int data type is %d\n", sizeof(int));
+    printf("Size of float data type is %d\n", sizeof(float));
+    printf("Size of char data type is %d\n", sizeof(char));
+    printf("Size of short data type is %d\n", sizeof(short));
+    printf("Size of long data type is %d\n", sizeof(long));
+    printf("Size of long long data type is %d\n", sizeof(long long));
+    printf("Size of double data type is %d\n", sizeof(double));
+
+
+    return 0;
+}
+    
+
---
+++ c programming/pointers in c/sizeof_2.c
@@ -0,0 +1,11 @@
+//Operand is an expression
+#include <stdio.h>
+
+int main()
+{
+    float x = 55.93;
+    double y = 398670.39;
+    printf("Size of (x+y) expression is: %d", sizeof(x + y));
+    
+    return 0;
+}
