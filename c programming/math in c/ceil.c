283048294a8ae0b46af0bd6e460a508589fb7214
goutamthakur <goutamthakur2508@gmail.com>
2021-09-27 14:26:29 +0530

    programs with math function in c added

---
+++ c programming/math in c/ceil.c
@@ -0,0 +1,16 @@
+//ceil function in c rounds up the given number
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a = 19.3;
+    float b = 29.9;
+    float c = 9.5;
+    printf("%f\n", ceil(a));
+    printf("%f\n", ceil(b));
+    printf("%f\n", ceil(c));
+
+  
+    return 0;
+}
---
+++ c programming/math in c/pow.c
@@ -0,0 +1,11 @@
+// pow function in c returns the power of given number
+//pow(base, exponent)
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    printf("%f\n", pow(2, 2));
+    printf("%f\n", pow(9,9));
+
+}
